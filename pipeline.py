35449100581d0698d2bcca243f380b0f4328e537
Owen Lennon <owenlennon3@gmail.com>
2019-04-21 19:06:35 +0100

    Pipeline file

---
+++ pipeline.py
@@ -0,0 +1,50 @@
+import os
+import fnmatch
+import gzip
+import bz2
+import re
+
+def gen_find(filepat, top):
+	for path , dirlist, filelist in os.walk(top):
+		for name in filelist:
+			yield os.path.join(path, name)
+
+def gen_opener(filenames):
+	for filename in filenames:
+		if filename.endswith('.gz'):
+			f = gzip.open(filename, 'rt')
+		elif filename.endswith('.bz2'):
+			f = bz2.open(filename, 'rt')
+		else:
+			f= open(filename, 'rt')
+		yield f
+
+		f.close()
+
+
+def gen_concatenate(iterators):
+
+	for it in iterators:
+		yield from it
+
+
+def gen_grep(pattern, lines):
+	pat = re.compile(pattern)
+	for line in lines:
+		if pat.search(line):
+			yield line
+
+listNumbers= open('../cust/in/customer6.csv', 'rt')
+filw= open('../cust/out/thecallsV6.csv', 'w')
+for number in listNumbers:
+	number = ''.join(number.split())
+	if len(number) >= 10: 
+		lognames = gen_find('', '.')
+		files = gen_opener(lognames)
+		lines = gen_concatenate(files)
+		pylines = gen_grep('(?i)'+ number, lines)
+		for line in pylines:
+			#print(line)
+			filw.write(line+"\n")
+			
+			
